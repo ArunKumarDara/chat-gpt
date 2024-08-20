0966df8e9e2f01ee82cf3c5954a5b466b2f16594
Arun Kumar <mymailarun32@gmail.com>
2024-08-20 14:52:36 +0530

    feat(server): create basic server using express

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
+const appRouter = require("./routes");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("chat gpt clone !!!!");
 });
+
+app.use("/app/v1", appRouter);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
