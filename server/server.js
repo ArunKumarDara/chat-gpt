abc075403f660546e9b2f6a667d8d0f685ba8423
Arun Kumar <mymailarun32@gmail.com>
2024-08-24 15:46:18 +0530

    feat(server): use cookie parser

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const appRouter = require("./routes");
@@ -7,6 +8,7 @@ const appRouter = require("./routes");
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 const port = process.env.PORT || 3000;
 
